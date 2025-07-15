579c55e697102a57cf9ee06e6d49883f46e3a4ff
Earl Syx Hernandez <hernandezearlsyxmail@gmail.com>
2025-07-15 22:27:22 +0800

    Add project files.

---
+++ Methods/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+//Methods
+// encapsulating code- putting code in 1 spot.
+// calling this code in just one line
+// allows to reuse our code, 
+// call our code more than once
+// keeps code simpler ,and cleaner and easier to read.
+
+// pass data into them
+// get method data out of them
+// gets lots of data out of them
+
+//best practices, DRY - Don't repeat yourself.
+
+Console.WriteLine();
+// use over and over again
+// call method all code in the method is excuted in one line.
+
---
+++ Methods/SampleMethods.cs
@@ -0,0 +1,31 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//creats vasic svtracuter of the class.
+//static, allows to make a call directly to the methods
+//without having to set thigns up first called instantitation.
+namespace Methods
+{
+ //public- not for permission, not for security
+ //-any body can use this.
+ // internal - any body that's in the project can use it.
+ //private- anyone inside of this scope can use it but no body else.
+ // can sound for security but not.
+ // purpose, not allow people to call thing they shoud not really call directly.
+    public static class SampleMethods
+    {
+       //create a method
+       // void is return value, void nothing, return nothing
+       // -send back from method
+       //Pascal method, name of method
+       //(), here dat you want to pass in.
+       // {} scope
+       public static void SayHi() //method signature
+        {
+            Console.WriteLine("Hello User");
+        }
+    }
+}
